6a7d192984fcc048976e59c6dc7b759c34ba9e46
kapuutri <kaaaruniaputriii@gmail.com>
2026-03-25 15:06:18 +0700

    task

---
+++ server/models/Task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: {
+    type: String,
+    enum: ['To Do', 'In Progress', 'Done'],
+    default: 'To Do'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/taskhub');
+
+app.use('/tasks', taskRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
